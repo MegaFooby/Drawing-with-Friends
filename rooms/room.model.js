7c7bf7191600d9fadef66c59b863086198264451
Sunny Nagam <sunnynagam1@gmail.com>
2020-12-10 01:33:39 -0700

    Added Rooms simple backend

---
+++ rooms/room.model.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const schema = new Schema({
+    name: { type: String, unique: true, required: true },
+    isPrivate: { type: Boolean, required: true },
+    users: { type: [String], required: true },
+    creatorUsername: { type: String, required: true },
+    createdDate: { type: Date, default: Date.now }
+});
+
+schema.set('toJSON', {
+    virtuals: true,
+    versionKey: false,
+    transform: function (doc, ret) {
+        delete ret._id;
+    }
+});
+
+module.exports = mongoose.model('Room', schema);
+console.log("Exported Room Model");
